953bebdf56e4c7826c8f7dc8ff37f68bcccb09c1
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2019-02-11 14:41:43 +0100

    new Gatsby site

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# kamiljozwikcom
+[![Netlify Status](https://api.netlify.com/api/v1/badges/af8ba7d5-226d-4f06-8e07-b970adbee9bf/deploy-status)](https://app.netlify.com/sites/kamiljozwik/deploys)
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`kamil`, `jozwik`, `react`]} />
+    <h1>Coming soon...</h1>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage

9d9345a7d1802fe22ab5e1ece7b288fc5c242dd4
Kamil Jozwik <kamiljozwikpl@gmail.com>
2019-03-07 22:10:46 +0100

    temp page

---
+++ src/components/Temp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const LandingPage = () => (
+  <section className="landing">
+    <div className="landing__box">
+      <p className="landing__box--title">New page coming  soon...</p>
+      <p className="landing__box--desc" />
+    </div>
+    <p className="landing__bottom">Kamil</p>
+  </section>
+);
+
+export default LandingPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Landing from '../components/Landing';
+import Temp from '../components/Temp';
 // import { GatsbyImage } from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`kamil`, `jozwik`, `react`]} />
-    <Landing />
+    <Temp />
     { /* TODO OTHER SECTIONS */ }
   </Layout>
 );
